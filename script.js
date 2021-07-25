22bf7bb35547fd36c366798c09280eca822beb63
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2021-07-25 17:07:44 +0700

    promise & fetch

--- script.js
+++ script.js
@@ -1,46 +1,87 @@
-$('.search-button').on('click', function() {
+// $('.search-button').on('click', function() {
 
-    $.ajax({
-        url : `http://www.omdbapi.com/?apikey=c8252c4f&s=${$('.input-keyword').val()}`,
+//     $.ajax({
+//         url : `http://www.omdbapi.com/?apikey=c8252c4f&s=${$('.input-keyword').val()}`,
     
-        success : results => {
-            const movies = results.Search;
-            if (movies != undefined) {
-                let cards = '';
-                movies.forEach(element => {
-                    cards += getCards(element);
-                })
-                $('.movies-container').html(cards)
+//         success : results => {
+//             const movies = results.Search;
+//             if (movies != undefined) {
+//                 let cards = '';
+//                 movies.forEach(element => {
+//                     cards += getCard(element);
+//                 })
+//                 $('.movies-container').html(cards)
         
-                // detail button click
-                $('.modal-detail-button').on('click', function () {
-                    $.ajax({
-                        url : `http://www.omdbapi.com/?apikey=c8252c4f&i=${$(this).data('imdbid')}`,
-                        success : element => {
-                            const movie_details = getMovieDetails(element)
-                            $('.modal-body').html(movie_details);
-                        },
-                        error : message => {
-                            console.log(message);
-                        }
+//                 // detail button click
+//                 $('.modal-detail-button').on('click', function () {
+//                     $.ajax({
+//                         url : `http://www.omdbapi.com/?apikey=c8252c4f&i=${$(this).data('imdbid')}`,
+//                         success : element => {
+//                             const movie_details = getMovieDetails(element)
+//                             $('.modal-body').html(movie_details);
+//                         },
+//                         error : message => {
+//                             console.log(message);
+//                         }
+//                     })
+//                 })
+//             } else {
+//                 const jumbotron = errorJumbotron(`"${$('.input-keyword').val()}" is not found.`)
+//                 $('.error-page').html(jumbotron)
+//             }
+//         },
+    
+//         error : e => {
+//             const jumbotron = errorJumbotron(e)
+//             $('.error-page').html(jumbotron)
+//         }
+//     });
+
+// })
+
+
+// fetch with vanilla javascript
+const searchbutton = document.querySelector('.search-button')
+searchbutton.addEventListener('click', function() {
+    
+    const query = document.querySelector('.input-keyword').value;
+    fetch(`http://www.omdbapi.com/?apikey=c8252c4f&s=${query}`)
+
+        .then(response => response.json())
+        .then(function(results) {
+            const movies = results.Search;
+            let cards = ''
+
+            // if movies is exist
+            if (movies != undefined){
+                document.querySelector('.error-page').innerHTML = '';
+                movies.forEach(element => cards += getCard(element));
+                document.querySelector('.movies-container').innerHTML = cards;
+
+                // detail button event listener
+                const detailbutton = document.querySelectorAll('.modal-detail-button');
+                detailbutton.forEach(button => {
+                    button.addEventListener('click', function() {
+                        fetch(`http://www.omdbapi.com/?apikey=c8252c4f&i=${this.dataset.imdbid}`)
+                            .then(response => response.json())
+                            .then(response => {
+                                const modal_details = getMovieDetails(response)
+                                document.querySelector('.modal-body').innerHTML = modal_details
+                            })
                     })
                 })
-            } else {
-                const jumbotron = errorJumbotron(`"${$('.input-keyword').val()}" is not found.`)
-                $('.error-page').html(jumbotron)
             }
-        },
-    
-        error : e => {
-            const jumbotron = errorJumbotron(e)
-            $('.error-page').html(jumbotron)
-        }
-    });
+
+            else {
+                const jumbotron = errorJumbotron(`"${query}" is not found.`);
+                document.querySelector('.error-page').innerHTML = jumbotron;
+            }
+        });
 
 })
 
 
-function getCards(element) {
+function getCard(element) {
     return `<div class="col-md-4 my-5">
                 <div class="card">
                     <img class="card-img-top" src="${element.Poster}" alt="${element.Title}">
@@ -80,6 +121,7 @@ function getMovieDetails(element) {
 }
 
 function errorJumbotron(message) {
+    document.querySelector('.movies-container').innerHTML = '';
     return `<div class="jumbotron jumbotron-fluid">
                 <div class="container">
                     <h1 class="display-4">Error!</h1>
@@ -87,3 +129,5 @@ function errorJumbotron(message) {
                 </div>
             </div>`;
 }
+
+new XMLHttpRequest().setRequestHeader
